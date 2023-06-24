bf41c1afe9d62768b00ba3960a6aeabfc84cd977
Dan Garro <dobetterwithdan@gmail.com>
2023-06-24 10:32:43 -0400

    set up db within app

---
+++ src/handlers/firestore.js
@@ -0,0 +1,13 @@
+import { getFirestore, setDoc } from 'firebase/firestore'
+import app from '../lib/firebase.config'
+
+const db = getFirestore(app)
+
+const Firestore = {
+  writeDoc: (...args) => {
+    return new Promise(resolve => {
+      try {
+      } catch (e) {}
+    })
+  }
+}
