b42d3c534e280df3bb3c331371a49447b3327f89
Dan Garro <dobetterwithdan@gmail.com>
2023-06-25 12:24:00 -0400

    tested and working with firebase

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,27 @@
-function Card ({ path, title }) {
+import { useMemo } from 'react'
+
+function Card ({ path, title, createdAt }) {
+  const timestamp = useMemo(() => {
+    // split used on string obj
+    const date = `${new Date(createdAt.seconds * 1000)}`.split(" ")
+    return `${date[1]} ${date[2]} ${date[3]}`
+  }, [])
   return (
     <div className='col mb-5'>
       <div className='card' style={{ width: '18rem' }}>
-        <img src={path} className='card-img-top' alt={title} />
+        <div
+          style={{
+            height: '220px',
+            backgroundImage: `url(${path})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}
+        ></div>
+        <h5 className='text-center mt-2 text-secondary text-capitalize'>{title}</h5>
+        <div className='d-flex justify-content-around py-2 text-secondary'>
+          <p>{timestamp}</p>
+          <i>@username</i>
+        </div>
       </div>
     </div>
   )
