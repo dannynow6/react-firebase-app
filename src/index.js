376e165c672e2b1b15ed937b4e1b2b8a3b32d19e
Dan Garro <dobetterwithdan@gmail.com>
2023-06-26 16:42:17 -0400

    set up routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -8,18 +8,26 @@ import reportWebVitals from './reportWebVitals'
 import Provider from './context/FirestoreContext'
 import AuthProvider from './context/AuthContext'
 
+function Stocks () {
+  return <h1>My Stock Images</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <Provider>
         <Router>
-          <App />
+          <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/stocks' element={<Stocks />} />
+          </Routes>
         </Router>
       </Provider>
     </AuthProvider>
   </React.StrictMode>
 )
+// Note: each Route takes path (specifying path) and element 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
